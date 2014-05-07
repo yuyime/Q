21ca000033ce2f74eea0055a17cd25acdf6ddbaa
yuyime <1609527596@qq.com>
2014-05-07 14:59:51 +0800

    Revert "处理OK按钮的显隐"
    This reverts commit 07859383fa509024b73d23fcb08d6ca49c258699.

--- QR_CodeScan/src/com/example/qr_codescan/MainActivity.java
+++ QR_CodeScan/src/com/example/qr_codescan/MainActivity.java
@@ -5,58 +5,36 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.method.KeyListener;
-import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	private final static int SCANNIN_GREQUEST_CODE = 1;
-	//account
-	private EditText iaccount ;
-	//pass
-	private EditText ipass ;
-	//okButton
-	private Button okButton ;
-	//二维码字符串值
-	private String QRString ;
-	
-	//private ImageView mImageView;
+	/**
+	 * 显示扫描结果
+	 */
+	private TextView mTextView ;
+	/**
+	 * 显示扫描拍的图片
+	 */
+	private ImageView mImageView;
 	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//setContentView(R.layout.activity_main);
+//		setContentView(R.layout.activity_main);
 		setContentView(R.layout.activity_main2);
 		
-		iaccount = (EditText) findViewById(R.id.account);
-		ipass = (EditText) findViewById(R.id.pass);
-		okButton = (Button) findViewById(R.id.okbutton);
-		if(iaccount.getText().toString().equals("")||ipass.getText().equals("")){
-			okButton.setEnabled(false);
-		}
-		
-		//iaccount.setKeyListener(new accountInputKeyListener());
-//		okButton.setOnClickListener(new OnClickListener() {
-//			@Override
-//			public void onClick(View v) {
-//				Intent intent = new Intent();
-//				intent.setClass(MainActivity.this, MipcaActivityCapture.class);
-//				intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-//				startActivityForResult(intent, SCANNIN_GREQUEST_CODE);
-//			}
-//		});
-		//mImageView = (ImageView) findViewById(R.id.qrcode_bitmap);
+//		mTextView = (TextView) findViewById(R.id.result); 
+//		mImageView = (ImageView) findViewById(R.id.qrcode_bitmap);
 //		
 		//点击按钮跳转到二维码扫描界面，这里用的是startActivityForResult跳转
 		//扫描完了之后调到该界面
-//		
+//		Button mButton = (Button) findViewById(R.id.button1);
 //		mButton.setOnClickListener(new OnClickListener() {
 //			
 //			@Override
@@ -85,41 +63,5 @@ public class MainActivity extends Activity {
 //			break;
 //		}
 //    }	
-	
-	
-	class accountInputKeyListener implements KeyListener{
-		
-		@Override
-		public boolean onKeyUp(View view, Editable text, int keyCode, KeyEvent event) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-		
-		@Override
-		public boolean onKeyOther(View view, Editable text, KeyEvent event) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-		
-		@Override
-		public boolean onKeyDown(View view, Editable text, int keyCode,
-				KeyEvent event) {
-//			if(!iaccount.getText().toString().equals("")&&!ipass.getText().equals("")){
-//				okButton.setEnabled(true);
-//			}
-			return false;
-		}
-		
-		@Override
-		public int getInputType() {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-		
-		@Override
-		public void clearMetaKeyState(View view, Editable content, int states) {
-			// TODO Auto-generated method stub
-			
-		}
-	}
+
 }
